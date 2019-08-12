a84e35d7bf7103451f7cef56211171c082d576f1
Claudia Yan <claudia.yan@ibm.com>
2019-08-12 17:19:49 -0400

    testing stuff

--- kab-cli/kabanero-mgmt/cmd/list.go
+++ kab-cli/kabanero-mgmt/cmd/list.go
@@ -16,7 +16,12 @@ limitations under the License.
 package cmd
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"time"
+
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -32,12 +37,56 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+
 		fmt.Println("list called")
+
+		// var endpoint string = "http://10.211.54.131/api/4.0/edges/edge-3/firewall/config"
+		// var endpoint string = "http://10.211.54.244:31000/KabCollections-1.0-SNAPSHOT/v1/collections"
+		var endpoint string = "http://9.12.77.136:9080/kabasec/login"
+
+		username, _ := cmd.Flags().GetString("username")
+		password, _ := cmd.Flags().GetString("password")
+
+		client := &http.Client{
+			Timeout: time.Second * 30,
+		}
+		// formData := url.Values{
+		// 	"gituser": {username},
+		// 	"gitpat":  {password},
+		// }
+
+		requestBody, _ := json.Marshal(map[string]string{"gituser": username, "gitpat": password})
+
+		req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(requestBody))
+		if err != nil {
+			fmt.Printf("Error : %s", err)
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+
+		// req.SetBasicAuth(username, password)
+
+		resp, err := client.Do(req)
+
+		if err != nil {
+			fmt.Printf("The HTTP request failed with error %s\n", err)
+		}
+
+		// data, _ := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
+
+		data := json.Decode(resp.Body)
+		// data = json.Decode(resp.Body)
+
+		fmt.Println(string(data))
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
+
+	listCmd.PersistentFlags().StringP("username", "u", "", "nsxmanager username")
+	listCmd.PersistentFlags().StringP("password", "p", "", "nsxmanagerpassword")
 
 	// Here you will define your flags and configuration settings.
 
